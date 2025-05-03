a4d3f939c698d4be4bc4796a6e885bff0bed70df
Heshawa De Silva <sharindu@accelaero.com>
2025-05-03 12:47:56 +0400

    Initial commit

---
+++ src/test/java/org/springboot/java17/purchaseOrder/PurchaseOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springboot.java17.purchaseOrder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
